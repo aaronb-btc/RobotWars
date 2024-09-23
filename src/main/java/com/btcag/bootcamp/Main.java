875a1313f903f4fe81d376caeb9addb8928fcc8b
Bleßmann <aaron.blessmann@btc-ag.com>
2024-09-23 07:44:32 +0200

    Added ability to choose username

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,11 +1,17 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("  ____            _               _    __        __                     \n" +
                 " |  _ \\    ___   | |__     ___   | |_  \\ \\      / /   __ _   _ __   ___ \n" +
                 " | |_) |  / _ \\  | '_ \\   / _ \\  | __|  \\ \\ /\\ / /   / _` | | '__| / __|\n" +
                 " |  _ <  | (_) | | |_) | | (_) | | |_    \\ V  V /   | (_| | | |    \\__ \\\n" +
-                " |_| \\_\\  \\___/  |_.__/   \\___/   \\__|    \\_/\\_/     \\__,_| |_|    |___/");
+                " |_| \\_\\  \\___/  |_.__/   \\___/   \\__|    \\_/\\_/     \\__,_| |_|    |___/\n\nEnter your username: ");
+        String username = scanner.next();
+        System.out.println("Welcome, " + username  + "!");
     }
+
 }
